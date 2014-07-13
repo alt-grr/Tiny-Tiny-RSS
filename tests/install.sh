80a14ee16fee0949bead6f9c724d2a09415f8436
Miłosz <milosz+github@sieradzki.it>
2014-07-13 22:58:34 +0200

    Make curl more verbose

--- tests/install.sh
+++ tests/install.sh
@@ -8,7 +8,7 @@ if [[ "$DB" == "mysql" ]]; then export DBUSER=root; fi
 curl --version
 
 # Test if install page is accessible
-curl --fail -s http://localhost/install/
+curl --fail -sv http://localhost/install/
 
 # Test configuration and database
 curl --fail -s http://localhost/install/ --data "op=testconfig&DB_TYPE=$DB&DB_USER=$DBUSER&DB_PASS=&DB_NAME=travis_ci_test&DB_HOST=localhost&DB_PORT=&SELF_URL_PATH=http%3A%2F%2Flocalhost%2F" | tee /dev/tty | awk '/Configuration check succeeded/ {f1=1} /Database test succeeded/ {f2=1} END {exit !(f1 && f2)}'
