0f55446827c72b1c1e21973a5120c5478297afd6
Miłosz <milosz+github@sieradzki.it>
2014-07-13 21:12:01 +0200

    Improve checking if installation succeeded

--- tests/install.sh
+++ tests/install.sh
@@ -7,14 +7,18 @@ if [[ "$DB" == "mysql" ]]; then export DBUSER=root; fi
 
 curl --version
 
-echo "Test if install page is accessible"
+# Test if install page is accessible
 curl --fail -s http://localhost/install/
 
-echo "Test configuration and database"
+# Test configuration and database
 curl --fail -s http://localhost/install/ --data "op=testconfig&DB_TYPE=$DB&DB_USER=$DBUSER&DB_PASS=&DB_NAME=travis_ci_test&DB_HOST=localhost&DB_PORT=&SELF_URL_PATH=http%3A%2F%2Flocalhost%2F" | awk '/Configuration check succeeded/ {f1=1} /Database test succeeded/ {f2=1} END {exit !(f1 && f2)}'
 
-echo "Initialize database"
+# Initialize database
 curl --fail -s http://localhost/install/ --data "op=installschema&DB_TYPE=$DB&DB_USER=$DBUSER&DB_PASS=&DB_NAME=travis_ci_test&DB_HOST=localhost&DB_PORT=&SELF_URL_PATH=http%3A%2F%2Flocalhost%2F" | grep -q "Database initialization completed"
 
-echo "Save config"
+# Save config
 curl --fail -s http://localhost/install/ --data "op=saveconfig&DB_TYPE=$DB&DB_USER=$DBUSER&DB_PASS=&DB_NAME=travis_ci_test&DB_HOST=localhost&DB_PORT=&SELF_URL_PATH=http%3A%2F%2Flocalhost%2F" | grep -q "Successfully saved config.php"
+
+# Check if main page is accesible and install page is not
+curl --fail -s http://localhost/install/ | grep -q "Error: config.php already exists in tt-rss directory; aborting."
+curl --fail -s http://localhost/ | awk '/Tiny Tiny RSS : Login/ {f1=1} /Fatal Error/ {f2=1} END {exit !(f1 && !f2)}'
