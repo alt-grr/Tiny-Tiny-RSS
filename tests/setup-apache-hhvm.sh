bccf58d61b0372cad1bf3e24c8a1e7db4fb765b5
Miłosz <milosz+github@sieradzki.it>
2014-07-13 23:27:07 +0200

    Fix HHVM running script

--- tests/setup-apache-hhvm.sh
+++ tests/setup-apache-hhvm.sh
@@ -8,7 +8,8 @@ sudo a2enmod rewrite actions fastcgi alias
 # Configure apache virtual hosts
 sudo cp -f tests/apache-hhvm.conf /etc/apache2/sites-available/default
 sudo sed -e "s?%TRAVIS_BUILD_DIR%?$(pwd)?g" --in-place /etc/apache2/sites-available/default
-sudo service apache2 restart
 
 # Run HHVM
 hhvm -m daemon -vServer.Type=fastcgi -vServer.Port=9000
+
+sudo service apache2 restart
--- tests/setup-apache-php.sh
+++ tests/setup-apache-php.sh
@@ -8,6 +8,8 @@ sudo apt-get install apache2 libapache2-mod-fastcgi
 sudo cp ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf.default ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf
 sudo a2enmod rewrite actions fastcgi alias
 echo "cgi.fix_pathinfo = 1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+
+# Run PHP
 ~/.phpenv/versions/$(phpenv version-name)/sbin/php-fpm
 
 # Configure apache virtual hosts
