0f07b194f99cb6cd7c7477bc0ded8b51a49ed449
Miłosz <milosz+github@sieradzki.it>
2014-07-14 01:07:37 +0200

    Add pgsql extension for HHVM

--- tests/setup-apache-hhvm.sh
+++ tests/setup-apache-hhvm.sh
@@ -10,6 +10,13 @@ sudo cp -f tests/apache-hhvm.conf /etc/apache2/sites-available/default
 sudo sed -e "s?%TRAVIS_BUILD_DIR%?$(pwd)?g" --in-place /etc/apache2/sites-available/default
 
 # Run HHVM
+mkdir ~/hhvm_extensions
+wget -O ~/hhvm_extensions/pgsql.so https://github.com/PocketRent/hhvm-pgsql/raw/releases/3.1.0/ubuntu/precise/pgsql.so
+sudo bash -c 'echo "DynamicExtensionPath = $HOME/hhvm_extensions
+DynamicExtensions {
+    * = pgsql.so
+}" >> /etc/hhvm/config.hdf'
+sudo bash -c 'echo "ADDITIONAL_ARGS=\"-c /etc/hhvm/config.hdf\"" >> /etc/default/hhvm'
 hhvm -m daemon -vServer.Type=fastcgi -vServer.Port=9000
 
 sudo service apache2 restart
