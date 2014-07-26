fe7957fdfdf62e99f07b09da75b6c8b720faed9f
Miłosz <milosz+github@sieradzki.it>
2014-07-26 15:58:38 +0200

    Change Tiny Tiny RSS log destination to syslog after installation

--- tests/install.sh
+++ tests/install.sh
@@ -19,6 +19,9 @@ curl --fail -s http://localhost/install/ --data "op=installschema&DB_TYPE=$DB&DB
 # Save config
 curl --fail -s http://localhost/install/ --data "op=saveconfig&DB_TYPE=$DB&DB_USER=$DBUSER&DB_PASS=&DB_NAME=travis_ci_test&DB_HOST=localhost&DB_PORT=&SELF_URL_PATH=http%3A%2F%2Flocalhost%2F" | tee /dev/tty | grep -q "Successfully saved config.php"
 
+# Change log destination to syslog
+sed -i -e "s/define\('LOG_DESTINATION', 'sql'\)/define\('LOG_DESTINATION', 'syslog'\)/" config.php
+
 # Check if main page is accesible and install page is not
 curl --fail -s http://localhost/install/ | tee /dev/tty | grep -q "Error: config.php already exists in tt-rss directory; aborting."
 
