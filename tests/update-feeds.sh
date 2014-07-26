452ce33ce9caee38cdcfc39cfdc529087cec41cb
Miłosz <milosz+github@sieradzki.it>
2014-07-26 17:58:18 +0200

    Force update of feeds

--- tests/update-feeds.sh
+++ tests/update-feeds.sh
@@ -2,5 +2,5 @@
 
 set -ev
 
-php update.php --feeds | tee /dev/tty > _feeds-update.log
+php update.php --force-update --feeds | tee /dev/tty > _feeds-update.log
 ! grep -q "^error" _feeds-update.log
